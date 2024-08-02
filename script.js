e4763aac503d6421b701890c27832f3743890407
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2024-08-02 14:58:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Example quote data
+
 // Example quote data
 const quotes = [
     { text: "The only limit to our realization of tomorrow is our doubts of today.", author: "Franklin D. Roosevelt" },
